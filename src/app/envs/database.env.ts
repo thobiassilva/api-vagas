258412ed9f5f68e0ff6be4d946b5c42e0aba363c
Darlan Fabricio <darlanfabricio@gmail.com>
2023-04-05 22:22:42 -0300

    config: add typeorm configuration

---
+++ src/app/envs/database.env.ts
@@ -0,0 +1,8 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+export const databaseEnv = {
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+};
---
+++ src/main/database/typeorm.connection.ts
@@ -0,0 +1,14 @@
+import { DataSource } from "typeorm";
+import config from "../config/typeorm.config";
+
+export class TypeormConnection {
+  private static _connection: DataSource;
+
+  public static async init() {
+    this._connection = await config.initialize();
+    console.log("Typeorm is ready.");
+  }
+  public static get connection() {
+    return this._connection;
+  }
+}
